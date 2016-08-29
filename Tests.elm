d99b255fb0633666aca01129f911dd5df7c14168
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2016-08-29 10:47:20 +0200

    Refactor the tests

--- Tests.elm
+++ Tests.elm
@@ -9,18 +9,22 @@ suite =
         [ describe "format"
             [ test "formats Danish phone numbers"
                 ( \_ ->
-                    Expect.equal (Just "34 56 12 45") (formatString "dk" "34561245")
+                    expectFormat "34 56 12 45" "dk" "34561245"
                 )
             , test "ignores superfluous digits"
                 ( \_ ->
-                    Expect.equal (Just "34 56 12 45") (formatString "dk" "34561245123")
+                    expectFormat "34 56 12 45" "dk" "34561245123"
                 )
             , test "fills in placeholders in place of missing digits"
                 ( \_ ->
-                    Expect.equal (Just "34 56 12 4 ") (formatString "dk" "3456124")
+                    expectFormat "34 56 12 4 " "dk" "3456124"
                 )
             ]
         ]
+
+
+expectFormat output country input =
+    Expect.equal (Just output) (formatString country input)
 
 
 main =
